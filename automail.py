774dc61a79057821616855b396fd19d685cef68d
Riyaz <riyazahamedsana@gmail.com>
2023-05-23 18:59:47 +0530

    First mini project

---
+++ automail.py
@@ -0,0 +1,23 @@
+import yagmail
+import os
+import datetime
+date = datetime.date.today().strftime("%B %d, %Y")
+path = 'Attendance'
+os.chdir(path)
+files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
+newest = files[-1]
+filename = newest
+sub = "Attendance Report for " + str(date)
+receiver = "riyazahamedsana@gmail.com"
+body = "Attendance report"
+# mail information
+yag = yagmail.SMTP("forclguse2@gmail.com", "jeynagodjcamknoq")
+
+# sent the mail
+yag.send(
+    to=receiver,
+    subject=sub, # email subject
+    contents=body,  # email body
+    attachments= filename  # file attached
+)
+print("Email Sent!")
